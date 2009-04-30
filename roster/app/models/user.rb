0ced3f0896bb5a201265aceb1f0ed9d22189b3c7
harley.trung@yale.edu <dtt22@dtt22-dell.(none)>
2009-04-30 16:46:32 -0400

    adding roster files first time

--- roster/app/models/user.rb
+++ roster/app/models/user.rb
@@ -1,3 +1,9 @@
 class User < ActiveRecord::Base
   has_many :comments
+  validates_presence_of :name
+  validates_presence_of :netid
+
+  def self.user_options
+      all.collect {|x| [x.name, x.id] }
+  end
 end
