2a23e238918641cfa13916f749e0ddba40f5de9c
harley.trung@yale.edu <dtt22@dtt22-dell.(none)>
2009-05-11 18:02:52 -0400

    second day of boot camp

--- roster/app/controllers/application.rb
+++ roster/app/controllers/application.rb
@@ -4,19 +4,21 @@
 class ApplicationController < ActionController::Base
   helper :all # include all helpers, all the time
   before_filter CASClient::Frameworks::Rails::Filter
-  before_filter :authorize_user
+
+  helper_method :current_user
+
+
   # See ActionController::RequestForgeryProtection for details
   # Uncomment the :secret if you're not using the cookie session store
   protect_from_forgery :secret => '767c8fe62c6588033782ff54b5a36038'
-  
-  def authorize_user
-    unless User.find_by_netid(session[:cas_user])
-      redirect_to 'http://yale.edu'
-    end
-  end  
-  
-  # See ActionController::Base for details 
+
+
+  def current_user
+    @current_user ||= User.find_by_netid(session[:cas_user]) || User.import_from_ldap(session[:cas_user], true)
+  end
+
+  # See ActionController::Base for details
   # Uncomment this to filter the contents of submitted sensitive data parameters
-  # from your application log (in this case, all fields with names like "password"). 
+  # from your application log (in this case, all fields with names like "password").
   # filter_parameter_logging :password
 end
