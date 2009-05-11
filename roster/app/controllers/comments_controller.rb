7235d2deae2171eac713cc3bc30715ea6f8b2b52
harley.trung@yale.edu <dtt22@dtt22-dell.(none)>
2009-05-11 15:30:23 -0400

    after reset

--- roster/app/controllers/comments_controller.rb
+++ roster/app/controllers/comments_controller.rb
@@ -2,15 +2,15 @@ class CommentsController < ApplicationController
   def index
     @comments = Comment.all
   end
-  
+
   def show
     @comment = Comment.find(params[:id])
   end
-  
+
   def new
     @comment = Comment.new
   end
-  
+
   def create
     @comment = Comment.new(params[:comment])
     if @comment.save
@@ -20,11 +20,11 @@ class CommentsController < ApplicationController
       render :action => 'new'
     end
   end
-  
+
   def edit
     @comment = Comment.find(params[:id])
   end
-  
+
   def update
     @comment = Comment.find(params[:id])
     if @comment.update_attributes(params[:comment])
@@ -34,7 +34,7 @@ class CommentsController < ApplicationController
       render :action => 'edit'
     end
   end
-  
+
   def destroy
     @comment = Comment.find(params[:id])
     @comment.destroy
