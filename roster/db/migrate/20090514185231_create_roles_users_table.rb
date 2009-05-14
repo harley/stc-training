a7e103fdb0d9f7e6a62bc45e4ed903f82fb99f03
Harley Trung <harley.trung@yale.edu>
2009-05-14 16:39:11 -0400

    Including id false in rolesusers table

--- roster/db/migrate/20090514185231_create_roles_users_table.rb
+++ roster/db/migrate/20090514185231_create_roles_users_table.rb
@@ -1,6 +1,6 @@
 class CreateRolesUsersTable < ActiveRecord::Migration
   def self.up
-    create_table :roles_users, :id => true do |t|
+    create_table :roles_users, :id => false do |t|
       t.integer :role_id
       t.integer :user_id
       t.timestamps
